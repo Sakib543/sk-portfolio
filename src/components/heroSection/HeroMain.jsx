6c63e0bd0ada480ab3c7da7ebe6d99cfb04cb98d
Sakib543 <alisakib543@gmail.com>
2025-06-03 01:19:50 +0500

    img change

--- src/components/heroSection/HeroMain.jsx
+++ src/components/heroSection/HeroMain.jsx
@@ -1,12 +1,12 @@
 import HeroText from "./HeroText";
-import Heropic from "./HeroPic"
+import HeroPic from "./HeroPic"
 
 const HeroMain = () => {
   return (
     <div className=" pt-40 pb-16">
       <div className="flex md:flex-row sm:flex-col  max-w-[1200px] mx-auto justify-between items-center relative px-4">
         <HeroText />
-        <Heropic />
+        <HeroPic />
       </div>
     </div>
   );
