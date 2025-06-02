0e737fd0cb28c08802f6773ebaf84cfdd90ce1fe
Sakib543 <alisakib543@gmail.com>
2025-06-03 01:44:06 +0500

    Add social media links

--- src/components/contactmesection/ContactSocial.jsx
+++ src/components/contactmesection/ContactSocial.jsx
@@ -5,9 +5,9 @@ import { FaInstagramSquare } from "react-icons/fa";
 const ContactSocial = () => {
   return (
     <div className="flex gap-4">
-      <SingleSocial Link="#" Icon={FaGithub} />
-      <SingleSocial Link="#" Icon={FaLinkedin} />
-      <SingleSocial Link="#" Icon={FaInstagramSquare} />
+      <SingleSocial Link="https://github.com/Sakib543" Icon={FaGithub} />
+      <SingleSocial Link="https://www.linkedin.com/in/saqib-ali-ba57681b5/" Icon={FaLinkedin} />
+      <SingleSocial Link="https://www.instagram.com/i_am_sakib543?igsh=d2JhNzdyZGJmM3V5" Icon={FaInstagramSquare} />
     </div>
   );
 };
